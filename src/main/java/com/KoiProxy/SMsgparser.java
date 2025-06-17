e59427055530f4c7294fb699397034be98bd8f37
ilyasrpl <ilyasrpl@gmail.com>
2025-06-17 16:53:19 +0700

    add PacketType

--- src/main/java/com/KoiProxy/SMsgparser.java
+++ src/main/java/com/KoiProxy/SMsgparser.java
@@ -21,6 +21,60 @@ public class SMsgparser {
       }
     }
 
+    if (packetName == "MSG") {
+      try {
+        result += "[MSG:" + packet.getDataInput().readUTF() + "]";
+        result += "[TYPE:" + packet.getDataInput().readByte() + "]";
+      } catch (java.io.IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    if (packetName == "LOGIN_OPTION") {
+      try {
+        byte totalAcc = packet.getDataInput().readByte();
+        for (int i = 0; i < totalAcc; ++i) {
+          int id = packet.getDataInput().readInt();
+          String name = packet.getDataInput().readUTF();
+          byte a1 = packet.getDataInput().readByte();
+          byte a2 = packet.getDataInput().readByte();
+          byte a3 = packet.getDataInput().readByte();
+          byte a4 = packet.getDataInput().readByte();
+
+          result += "[ID:" + id + "]";
+          result += "[NAME:" + name + "]";
+
+          byte[] var13 = new byte[12];
+
+          int var14;
+          for (var14 = 0; var14 < var13.length; ++var14) {
+            var13[var14] = -1;
+          }
+
+          byte var15;
+          for (var14 = 0; var14 < a4; ++var14) {
+            var15 = packet.getDataInput().readByte();
+            byte var16 = packet.getDataInput().readByte();
+            var13[var15] = var16;
+          }
+
+          short var19 = packet.getDataInput().readShort();
+          var15 = packet.getDataInput().readByte();
+          packet.getDataInput().readByte();
+          packet.getDataInput().readByte();
+          short var18;
+          if ((var18 = packet.getDataInput().readShort()) >= 0) {
+            String a5 = packet.getDataInput().readUTF();
+            byte a6 = packet.getDataInput().readByte();
+            result += "[CLAN:" + a5 + "]";
+          }
+          // todo
+        }
+      } catch (java.io.IOException e) {
+        e.printStackTrace();
+      }
+    }
+
     return result;
   }
 }
--- src/main/java/com/KoiProxy/SPacketType.java
+++ src/main/java/com/KoiProxy/SPacketType.java
@@ -2,7 +2,9 @@ package com.KoiProxy;
 
 public class SPacketType {
   packetMsg packetmsgs[] = {
-    new packetMsg((byte) -40, "INIT")
+    new packetMsg((byte) -40, "INIT"),
+    new packetMsg((byte) 53, "MSG"),
+    new packetMsg((byte) 13, "LOGIN_OPTION")
   };
 
   public String getPacketName(byte packetId) {
